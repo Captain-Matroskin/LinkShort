6bb7b7bfcf17ea871d195c2ad1b5917bf0250c04
Matroskin <matroskin771@mail.ru>
2022-10-04 05:58:44 +0300

    add config, viper

--- build/setUp.go
+++ build/setUp.go
@@ -1,10 +1,20 @@
 package build
 
 import (
+	"LinkShortening/config"
 	api "LinkShortening/internals/linkShort/api"
 	"LinkShortening/internals/linkShort/application"
 	"LinkShortening/internals/linkShort/orm"
 	apiMiddle "LinkShortening/internals/middleware/api"
+	errPkg "LinkShortening/internals/myerror"
+	"github.com/spf13/viper"
+)
+
+const (
+	ConfNameMain = "main"
+	ConfNameDB   = "database"
+	ConfType     = "yml"
+	ConfPath     = "./config/"
 )
 
 type installSetUp struct {
@@ -39,4 +49,45 @@ func SetUp(connectionDB orm.ConnectionInterface) *installSetUp {
 	result.LinkShortManager = linkShortManager
 
 	return &result
+}
+
+func InitConfig() (error, []interface{}) {
+	viper.AddConfigPath(ConfPath)
+	viper.SetConfigType(ConfType)
+
+	viper.SetConfigName(ConfNameMain)
+	errRead := viper.ReadInConfig()
+	if errRead != nil {
+		return &errPkg.MyErrors{
+			Text: errRead.Error(),
+		}, nil
+	}
+	mainConfig := config.MainConfig{}
+	errUnmarshal := viper.Unmarshal(&mainConfig)
+	if errUnmarshal != nil {
+		return &errPkg.MyErrors{
+			Text: errUnmarshal.Error(),
+		}, nil
+	}
+
+	viper.SetConfigName(ConfNameDB)
+	errRead = viper.ReadInConfig()
+	if errRead != nil {
+		return &errPkg.MyErrors{
+			Text: errRead.Error(),
+		}, nil
+	}
+	dbConfig := config.DBConfig{}
+	errUnmarshal = viper.Unmarshal(&dbConfig)
+	if errUnmarshal != nil {
+		return &errPkg.MyErrors{
+			Text: errUnmarshal.Error(),
+		}, nil
+	}
+
+	var result []interface{}
+	result = append(result, mainConfig)
+	result = append(result, dbConfig)
+
+	return nil, result
 }
--- config/config.go
+++ config/config.go
@@ -7,3 +7,18 @@ type Database struct {
 	Password   string
 	SchemaName string
 }
+
+type DBConfig struct {
+	Db Database `mapstructure:"db"`
+}
+
+type MainConfig struct {
+	Main Main `mapstructure:"main"`
+}
+
+type Main struct {
+	HostGrpc string
+	PortGrpc string
+	Network  string
+	PortHttp string
+}
